a65b7a9095633c314b581e6f5c118c26b65fd895
Tschrock <tschrock123@gmail.com>
2019-05-12 01:05:45 -0400

    Add == overrides for Token and CharacterPosition

--- Komatiite/CharacterPosition.cs
+++ Komatiite/CharacterPosition.cs
@@ -1,46 +1,78 @@
 namespace Komatiite
 {
 
-public class CharacterPosition
-{
-    public CharacterPosition(int column, int row, int index)
+    public class CharacterPosition
     {
-        Column = column;
-        Row = row;
-        Index = index;
-    }
-
-    public int Column { get; set; }
-
-    public int Row { get; set; }
-
-    public int Index { get; set; }
-
-    public void BumpRow()
-    {
-        this.Column = 0;
-        this.Row = this.Row + 1;
-    }
-
-    public void BumpForChar(int c)
-    {
-        if (c != -1)
+        public CharacterPosition(int column, int row, int index)
         {
-            if (c == '\n')
-            {
-                BumpRow();
-            }
-            Index = Index + 1;
+            Column = column;
+            Row = row;
+            Index = index;
         }
-    }
 
-    public CharacterPosition Clone()
-    {
-        return new CharacterPosition(this.Column, this.Row, this.Index);
-    }
+        public int Column { get; set; }
 
-    public static CharacterPosition Empty = new CharacterPosition(-1, -1, -1);
+        public int Row { get; set; }
 
+        public int Index { get; set; }
+
+        public void BumpRow()
+        {
+            this.Column = 0;
+            this.Row = this.Row + 1;
+        }
+
+        public void BumpForChar(int c)
+        {
+            if (c != -1)
+            {
+                if (c == '\n')
+                {
+                    BumpRow();
+                }
+                Index = Index + 1;
+            }
+        }
+
+        public CharacterPosition Clone()
+        {
+            return new CharacterPosition(this.Column, this.Row, this.Index);
+        }
+
+        public static CharacterPosition Empty = new CharacterPosition(-1, -1, -1);
+
+        public static bool operator ==(CharacterPosition left, CharacterPosition right)
+        {
+            if (object.ReferenceEquals(null, left))
+                return object.ReferenceEquals(null, right);
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(CharacterPosition left, CharacterPosition right)
+        {
+            if (object.ReferenceEquals(null, left))
+                return !object.ReferenceEquals(null, right);
+            return !left.Equals(right);
+        }
+
+        public override bool Equals(object other)
+        {
+            return Equals(other as CharacterPosition);
+        }
+
+        public bool Equals(CharacterPosition other)
+        {
+            return other != null && Column == other.Column && Row == other.Row && Index == other.Index;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 352033288;
+            hashCode = hashCode * -1521134295 + Column.GetHashCode();
+            hashCode = hashCode * -1521134295 + Row.GetHashCode();
+            hashCode = hashCode * -1521134295 + Index.GetHashCode();
+            return hashCode;
+        }
 
     }
     
--- Komatiite/Token.cs
+++ Komatiite/Token.cs
@@ -1,41 +1,73 @@
+using System;
+using System.Text;
 
 namespace Komatiite
 {
 
-using System.Text;
-
-public class Token
-{
-    public Token(TokenType type)
+    public class Token : IEquatable<Token>
     {
-        TokenType = type;
-        StartPosition = CharacterPosition.Empty;
-        EndPosition = CharacterPosition.Empty;
-    }
-    public Token(TokenType type, CharacterPosition startPosition)
-    {
-        TokenType = type;
-        StartPosition = startPosition;
-        EndPosition = startPosition;
-    }
-    public Token(TokenType type, CharacterPosition startPosition, CharacterPosition endPosition)
-    {
-        TokenType = type;
-        StartPosition = startPosition;
-        EndPosition = endPosition;
-    }
-
-    public TokenType TokenType { get; set; }
-    public CharacterPosition StartPosition { get; set; }
-    public CharacterPosition EndPosition { get; set; }
-    public int Length
-    {
-        get
+        public Token(TokenType type)
         {
-            return EndPosition.Index - StartPosition.Index;
+            TokenType = type;
+            StartPosition = CharacterPosition.Empty;
+            EndPosition = CharacterPosition.Empty;
         }
-    }
+        public Token(TokenType type, CharacterPosition startPosition)
+        {
+            TokenType = type;
+            StartPosition = startPosition;
+            EndPosition = startPosition;
+        }
+        public Token(TokenType type, CharacterPosition startPosition, CharacterPosition endPosition)
+        {
+            TokenType = type;
+            StartPosition = startPosition;
+            EndPosition = endPosition;
+        }
 
+        public TokenType TokenType { get; set; }
+        public CharacterPosition StartPosition { get; set; }
+        public CharacterPosition EndPosition { get; set; }
+        public int Length
+        {
+            get
+            {
+                return EndPosition.Index - StartPosition.Index;
+            }
+        }
+
+        public static bool operator ==(Token left, Token right)
+        {
+            if (object.ReferenceEquals(null, left))
+                return object.ReferenceEquals(null, right);
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Token left, Token right)
+        {
+            if (object.ReferenceEquals(null, left))
+                return !object.ReferenceEquals(null, right);
+            return !left.Equals(right);
+        }
+
+        public override bool Equals(object other)
+        {
+            return Equals(other as Token);
+        }
+
+        public bool Equals(Token other)
+        {
+            return other != null && TokenType == other.TokenType && StartPosition == other.StartPosition && EndPosition == other.EndPosition;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 352033288;
+            hashCode = hashCode * -1521134295 + TokenType.GetHashCode();
+            hashCode = hashCode * -1521134295 + StartPosition.GetHashCode();
+            hashCode = hashCode * -1521134295 + EndPosition.GetHashCode();
+            return hashCode;
+        }
 
     }
 
